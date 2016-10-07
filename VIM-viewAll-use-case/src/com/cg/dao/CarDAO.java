abc27318cda78d9d690ba134b8cc7cf3cb104b44
Anand Kumar Singh <anandkumarsingh808@gmail.com>
2016-10-07 17:55:00 +0530

    Add files via upload

---
+++ VIM-viewAll-use-case/src/com/cg/dao/CarDAO.java
@@ -0,0 +1,24 @@
+package com.cg.dao;
+
+import java.util.*;
+
+import com.cg.dao.Impl.JDBCDaoException;
+import com.cg.dto.*;
+
+//Follow TODOs (if available)
+/**
+ * 
+ * This is a CarDAO class
+ * @see java.lang.Object
+ * @author Abhishek
+ * 
+ *
+ */
+public interface CarDAO 
+{
+    public List<CarDTO> findAll() throws JDBCDaoException; 
+    public CarDTO findById(int id) throws JDBCDaoException;
+    public void create(CarDTO car) throws JDBCDaoException;
+    public void update(CarDTO car) throws JDBCDaoException;
+    public void delete(String[] ids) throws JDBCDaoException;
+}
